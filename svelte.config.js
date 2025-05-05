3e40f6784b63623f9388dbcc7cb8d2fa860f7774
yuchanahn <ultrauc123@gmail.com>
2025-05-06 00:01:30 +0900

    update

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,8 @@ import adapter from '@sveltejs/adapter-static';
 export default {
 	kit: {
 		adapter: adapter({
-			strict: false
+			strict: false,
+			fallback: 'index.html'
 		}),
 		paths: {
 			base: '/personaxi-front' // 너의 리포지토리 이름이랑 정확히 일치해야 함
