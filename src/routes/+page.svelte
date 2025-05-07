52b8b2703563055dc80c8d5f1ec7257a58808e3b
yuchanahn <ultrauc123@gmail.com>
2025-05-07 22:31:26 +0900

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
 
 	onMount(async () => {
 		try {
-			const res = await fetch('http://localhost:8080/api/user/me', {
+			const res = await fetch('https://localhost:8080/api/user/me', {
 				credentials: 'include'
 			});
 			if (res.ok) {
@@ -20,7 +20,7 @@
 {#if user}
 	<p>👋 안녕하세요, {user.name} 님!</p>
 {:else}
-	<a href="http://localhost:8080/auth/google/login">
+	<a href="https://localhost:8080/auth/google/login">
 		<button>로그인</button>
 	</a>
 {/if}
