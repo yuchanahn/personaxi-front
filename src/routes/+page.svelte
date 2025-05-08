e0b49e66a88b158ac88044e591aaf50a67cc92bb
yuchanahn <ultrauc123@gmail.com>
2025-05-08 14:58:05 +0900

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,36 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
+	import { onMount } from "svelte";
 
-	let user: { name: string, email: string } | null = null;
-
+	let user: { name: string; email: string } | null = null;
 
 	onMount(async () => {
 		try {
-			const res = await fetch('http://localhost:8080/api/user/me', {
-				credentials: 'include',
+			const res = await fetch("http://localhost:8080/api/user/me", {
+				credentials: "include",
 			});
 			if (res.ok) {
 				user = await res.json();
 			}
 		} catch (e) {
-			console.error('Not logged in');
+			console.error("Not logged in");
 		}
 	});
 
 	const login = async () => {
-    	window.location.href = 'http://localhost:8080/auth/google/login';
+		try {
+			const response = await fetch(
+				"http://localhost:8080/auth/google/login",
+				{
+					method: "GET",
+					credentials: "include", // 쿠키 포함
+				},
+			);
+			if (response.redirected) {
+				window.location.href = response.url; // 리다이렉트 처리
+			}
+		} catch (error) {
+			console.error("Error:", error);
+		}
 	};
 </script>
 
@@ -28,5 +40,6 @@
 	<button
 		on:click={login}
 		class="bg-blue-500 text-white font-bold py-2 px-4 rounded"
-	>로그인</button>
+		>로그인</button
+	>
 {/if}
