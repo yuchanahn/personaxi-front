5e427a746a623f8945dce219e614abfbe167a0d7
yuchanahn <ultrauc123@gmail.com>
2025-06-24 11:33:25 +0900

    update-localserver

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -4,4 +4,5 @@ const host = window.location.hostname;
 export const API_BASE_URL =
     host === "localhost" || host === "127.0.0.1"
         ? "http://localhost:8080"
-        : "https://7a87-175-115-55-94.ngrok-free.app";
+        : "http://localhost:8080";
+//: "https://7a87-175-115-55-94.ngrok-free.app";
